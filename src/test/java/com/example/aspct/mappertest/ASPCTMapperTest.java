3a3457f83ac381f74f600f7c7f76b66f8a1687f5
R4X05Z3R0 <razeroeuw@gmail.com>
2026-05-14 15:08:40 +0200

    structure for tests implemented v2

--- src/test/java/com/example/aspct/mappertest/ASPCTMapperTest.java
+++ src/test/java/com/example/aspct/mappertest/ASPCTMapperTest.java
@@ -1,10 +1,10 @@
-package com.example.aspct.integration;
+package com.example.aspct.mappertest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ASPCTIntegrationTest {
+public class ASPCTMapperTest {
     @Test
     void contextLoads(){
 
